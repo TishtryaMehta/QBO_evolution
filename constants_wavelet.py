54dbcd375691980cc091ee165702e6a86025e44d
Tishtrya Mehta <35487135+TaraAthem@users.noreply.github.com>
2023-07-19 15:56:48 +0100

    Delete constants_wavelet.py

--- constants_wavelet.py
+++
@@ -1,6 +0,0 @@
-import os
-
-#constants_wavelet.py
-
-DATA_PATH =  os.path.join("/", "home", "space", "phrsnz", "Desktop", "Academic", "Programs","PhD", "QBO_evolution", "Data_processed", "Azimuthal", "Full_data")
-SAVE_PATH = os.path.join("/", "home", "space", "phrsnz", "Desktop", "Academic", "Programs","PhD", "QBO_evolution", "CWT_analysis_fmodes", "Output")
